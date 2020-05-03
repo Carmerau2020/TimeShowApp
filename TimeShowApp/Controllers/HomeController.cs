ac8222878e71742fc5c68bf0d63b09b04793f5c1
Carmerau2020 <jaworska.katarzyna81@gmail.com>
2020-05-03 17:30:45 +0100

    Initial commit

---
+++ TimeShowApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeShowApp.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+                
+        public ActionResult LocalTime()
+        {
+            ViewBag.Message = "Your local time is:";
+
+            return View();
+        }
+
+        public ActionResult ServerTime()
+        {
+            ViewBag.Message = "UTC server time is:";
+
+            return View();
+        }
+    }
+}
---
+++ TimeShowApp/Scripts/dataUtcSet.js
@@ -0,0 +1,24 @@
+﻿setInterval(function () {
+    var seconds = new Date().getUTCSeconds();
+    var sdegree = seconds * 6;
+    var srotate = "rotate(" + sdegree + "deg)";
+    $("#sec").css({ "transform": srotate });
+}, 1000);
+
+setInterval(function () {
+    var hours = new Date().getUTCHours();
+    var mins = new Date().getUTCMinutes();
+    var hdegree = hours * 30 + (mins / 2);
+    var hrotate = "rotate(" + hdegree + "deg)";
+
+    $("#hour").css({ "transform": hrotate });
+}, 1000);
+
+setInterval(function () {
+
+    var mins = new Date().getUTCMinutes();
+    var mdegree = mins * 6;
+    var mrotate = "rotate(" + mdegree + "deg)";
+
+    $("#min").css({ "transform": mrotate });
+}, 1000);
